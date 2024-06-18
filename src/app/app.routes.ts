f4300687791ebd809709d92eecf7f879257b11d3
roshanpradhan <pradhanr@vcu.edu>
2024-06-18 00:35:34 -0400

    adding routing and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,30 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
+
+  {
+    path: 'home',
+    loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./components/auth/login/login.component').then(m => m.LoginComponent),
+  },
+  {
+    path: 'address',
+    loadComponent: () => import('./components/address/address-list/address-list.component').then(m => m.AddressListComponent)
+  },
+  {
+    path: 'page-not-found',
+    loadComponent: () => import('./shared/page-not-found/page-not-found.component').then(m => m.PageNotFoundComponent)
+  },
+  // {
+  //   path: 'auth',
+  //   loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule)
+  // },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'page-not-found' }
+    // { path: 'login', component: LoginComponent },
     // { path: 'logout', component: LoginComponent },
     // { path: 'addresses', component: AddressListComponent },
     // { path: 'address/:id', component: AddressDetailComponent },
